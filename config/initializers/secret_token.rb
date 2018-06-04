c95efa4439bc0887ad195af50126eedce1caca76
Tanay Upadhyaya <tanayupadhyaya@gmail.com>
2018-06-04 17:15:55 +0530

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-RailsBlog::Application.config.secret_key_base = 'caf90e0d4683ead857beecb8663437fae0677f0d239e9f54e57f47e109feb00eff5047a2ffc904232b9fdf29e63f947d101fb9e758652073eec992fcf18d1fd4'
+RailsBlog::Application.config.secret_key_base = 'xyz'
